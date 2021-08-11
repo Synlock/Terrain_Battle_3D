7aa76da69a51e2ab70e70394ea3091dbf21403cc
Synlock <65950705+Synlock@users.noreply.github.com>
2021-08-11 18:44:24 +0300

    a

--- Assets/_Game/Scripts/Scene Controllers/LoadScenesManager.cs
+++ Assets/_Game/Scripts/Scene Controllers/LoadScenesManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScenesManager : MonoBehaviour
@@ -7,12 +8,22 @@ public class LoadScenesManager : MonoBehaviour
 
     void Start()
     {
-        if(SceneManager.GetActiveScene().buildIndex == 0)
-            StartGame();
+        if (SceneManager.GetActiveScene().buildIndex == 0 && !SceneManager.GetSceneByBuildIndex(currentLevel).isLoaded)
+        {
+            if(PlayerPrefs.GetInt("Current Level") == 0)
+            {
+                currentLevel = 1;
+            }
+            else currentLevel = PlayerPrefs.GetInt("Current Level");
+
+            StartCoroutine(StartGame());
+        }
     }
-    public void StartGame()
+    IEnumerator StartGame()
     {
         SceneManager.LoadScene(currentLevel, LoadSceneMode.Additive);
+        yield return new WaitForSeconds(0.01f);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(currentLevel));
     }
     public void LoadNextScene()
     {
@@ -23,6 +34,8 @@ public class LoadScenesManager : MonoBehaviour
         if (currentLevel < 2)
             SceneManager.LoadScene(currentLevel + 1);
         else SceneManager.LoadScene(currentLevel);
+
+        PlayerPrefs.SetInt("Current Level", currentLevel);
 
         SceneManager.LoadScene("Main Menu", LoadSceneMode.Additive);
     }
